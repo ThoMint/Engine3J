094d29fa3f6a8731fa4ef69ecb69f2510958d314
Thomas <thomas.hofmann2003@gmail.com>
2019-05-30 18:44:26 +0200

    CUBES!
    Added an Arraylist of cubes which size can be set in the code

--- src/thomas/hofmann/Engine.java
+++ src/thomas/hofmann/Engine.java
@@ -2,6 +2,7 @@ package thomas.hofmann;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.util.ArrayList;
 
 import javax.swing.JPanel;
 
@@ -9,9 +10,21 @@ public class Engine extends JPanel {
 
 	private static final long serialVersionUID = -8298272391915136927L;
 
-	private Mesh cube;
+	private ArrayList<Mesh> world;
+	private Mesh temp;
+	private double x = 0.0;
+	private final int cubes = 21;
+
 	private Camera cam;
 	private boolean draw = false;
+
+	private final double FPS = 60;
+	private long start = System.nanoTime();
+	private long end = 0;
+	private long diff = 0;
+	private long wait = 0;
+	private int fps = 0;
+	private int frameCounter = (int) (FPS);
 
 	public Engine(Color background, Window w, double fieldOfView, double zNear, double zFar) {
 		super();
@@ -26,7 +39,9 @@ public class Engine extends JPanel {
 	}
 
 	private void init() {
-		cube = new Mesh(new double[][][] { 
+		world = new ArrayList<>();
+		for(int i=0;i<cubes;i++) {
+				temp = new Mesh(new double[][][] { 
 				{ { -1.0, -1.0, -1.0 }, { -1.0, 1.0, -1.0 }, { 1.0, 1.0, -1.0 } },
 				{ { -1.0, -1.0, -1.0 }, { 1.0, 1.0, -1.0 }, { 1.0, -1.0, -1.0 } },
 				{ { 1.0, -1.0, -1.0 }, { 1.0, 1.0, -1.0 }, { 1.0, 1.0, 1.0 } },
@@ -39,22 +54,44 @@ public class Engine extends JPanel {
 				{ { -1.0, 1.0, -1.0 }, { 1.0, 1.0, 1.0 }, { 1.0, 1.0, -1.0 } },
 				{ { 1.0, -1.0, 1.0 }, { -1.0, -1.0, 1.0 }, { -1.0, -1.0, -1.0 } },
 				{ { 1.0, -1.0, 1.0 }, { -1.0, -1.0, -1.0 }, { 1.0, -1.0, -1.0 } } });
-		cube.setCamera(cam);
-		cube.translate(0, 0, 3);
+				temp.setCamera(cam);
+				temp.translate(i*2-(cubes-1), 0.0, cubes+5);
+				world.add(temp);
+		}
 	}
 
 	@Override
 	public void paint(Graphics g) {
 		super.paint(g);
 		if (draw) {
-			drawBackground(g);
-			cube.render(g);
-			repaint();
+			for(Mesh m : world) {
+				m.rotate(x, 0, 0);
+				m.render(g);
+			}
+			x+=0.01;
 		}
+		frameRate(g);
+		repaint();
 	}
 
-	private void drawBackground(Graphics g) {
-		g.setColor(Color.white);
-		g.fillRect(0, 0, getWidth(), getHeight());
+	private void frameRate(Graphics g) {
+		if(frameCounter>FPS) {
+			end = System.nanoTime();
+			diff = end-start;
+			wait = (long) ((1000.0 / FPS)-(diff/1000000000.0));
+			if(wait<1) {
+				wait=0;
+			}
+			fps = (int) (1000000000*FPS/diff);
+			start = System.nanoTime();
+			frameCounter=0;
+		}
+		try {
+			Thread.sleep(wait);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		g.drawString(String.valueOf(fps), 0, 10);
+		frameCounter++;
 	}
 }
